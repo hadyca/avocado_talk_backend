c8b8d39d38329ae73ff9ccc6d309f2a699b8e05b
jehyung <hadycas@gmail.com>
2021-08-30 15:53:38 +0900

    인증번호 요청 진행 중

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -6,51 +6,62 @@ export default {
   Mutation: {
     createAccount: async (_, { username, email, password }) => {
       try {
-        const existingUsername = await client.user.findFirst({
-          where: {
-            username,
-          },
-        });
-        if (existingUsername) {
-          throw new Error("이미 사용중인 유저이름이 있습니다.");
-        }
         const existingEmail = await client.user.findFirst({
           where: {
             email,
           },
         });
         if (existingEmail) {
-          throw new Error("이미 사용 중인 Email주소가 있습니다.");
+          return {
+            ok: false,
+            error:
+              "이미 사용중인 이메일 주소가 있습니다. 다른 이메일 주소를 입력해주세요.",
+          };
+        }
+        const existingUsername = await client.user.findFirst({
+          where: {
+            username,
+          },
+        });
+
+        if (existingUsername) {
+          return {
+            ok: false,
+            error:
+              "이미 사용중인 유저명이 있습니다. 다른 유저명을 사용해보세요!",
+          };
         }
         const uglyPassword = await bcrypt.hash(password, 10);
-        const newUser = await client.user.create({
+        await client.user.create({
           data: {
             username,
             email,
             password: uglyPassword,
           },
         });
+
+        //인증번호 요청 로직
         const loginSecret = generateSecret(111111, 999999);
         await sendSecretMail(email, loginSecret);
-        const updatedUser = await client.user.update({
+        await client.user.update({
           where: { email },
-          data: { loginSecret },
+          data: { loginSecret, proceedState: 1 },
         });
-        //인증번호 안넣으면 1시간 지나면 계정이 삭제됨.
+        //인증번호 안넣고 10분 지나면 계정이 삭제됨.
         setTimeout(async () => {
           const newUser = await client.user.findUnique({
             where: {
               email,
             },
           });
-          if (newUser.secretConfirm === false) {
+          if (newUser.proceedState === 1) {
             await client.user.delete({
               where: {
                 id: newUser.id,
               },
             });
           }
-        }, 60 * 60 * 1000);
+        }, 10 * 1000);
         return {
           ok: true,
         };
