78aa695f5e341cd27ef3fbcd628c7c95ed1f5c90
jehyung <hadycas@gmail.com>
2022-01-24 18:00:58 +0900

    fdsfds

--- userPostComments/seeUserPostComments/seeUserPostComments.resolvers.js
+++ userPostComments/seeUserPostComments/seeUserPostComments.resolvers.js
@@ -6,6 +6,9 @@ export default {
       client.userPostComment.findMany({
         where: { userPostId },
         include: { user: true },
+        orderBy: {
+          createdAt: "asc",
+        },
       }),
   },
 };
