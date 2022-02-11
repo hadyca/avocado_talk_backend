a0e276ca5ea295b431d11719d92f2a8fae734187
thatBro <hadycas@gmail.com>
2022-02-11 17:32:36 +0900

    remove all cache

--- Companies/companies.utils.js
+++
@@ -1,8 +0,0 @@
-import client from "../client";
-
-export const getUserCompany = async (userId) => {
-  const company = await client.company.findUnique({
-    where: { userId },
-  });
-  return company;
-};
--- Companies/seeCompany/seeCompany.typeDefs.js
+++
@@ -1,7 +0,0 @@
-import { gql } from "apollo-server";
-
-export default gql`
-  type Query {
-    seeCompany(companyId: Int): Company
-  }
-`;
